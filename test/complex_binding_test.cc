fad814034cc5a24cc63e2c365c5ca8671246dac3
Damian Momot <momotdamian@gmail.com>
2014-11-15 15:29:04 +0100

    Test for type-to-type binding where target type is bound by instance

--- test/complex_binding_test.cc
+++ test/complex_binding_test.cc
@@ -37,3 +37,21 @@ TEST(complex_binding, cycle) {
   EXPECT_EQ(if3, impl3);
   EXPECT_EQ(if1, impl3->intf1p_->Get());
 }
+
+TEST(complex_binding, type_binding_with_instance_target) {
+  cppdi::InjectorFactory factory;
+
+  shared_ptr<Implementation> impl(new Implementation());
+
+  shared_ptr<Injector> injector = factory.Create([&](Binder *binder) {
+    binder->BindInstance<shared_ptr<Implementation>>(impl);
+    binder->BindTypes<Interface, Implementation>();
+  });
+  DisposeGuard guard(injector);
+
+  EXPECT_FALSE(!injector->GetInstance<shared_ptr<Implementation>>());
+  EXPECT_FALSE(!injector->GetInstance<shared_ptr<Interface>>());
+  EXPECT_EQ(impl, injector->GetInstance<shared_ptr<Implementation>>());
+  EXPECT_EQ(injector->GetInstance<shared_ptr<Implementation>>(), injector->GetInstance<shared_ptr<Interface>>());
+  EXPECT_EQ(injector->GetInstance<shared_ptr<Provider<shared_ptr<Implementation>>>>()->Get(), injector->GetInstance<shared_ptr<Provider<shared_ptr<Interface>>>>()->Get());
+}
